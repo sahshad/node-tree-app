8035d9a92bf007993d2bdde4660cbb066db1f397
shahzad <mohmdshahzad2.0@gmail.com>
2025-08-17 15:06:04 +0530

    fixed node collection field typo mistake childlcount to childcount

--- backend/src/models/Node.ts
+++ backend/src/models/Node.ts
@@ -3,7 +3,7 @@ import { Schema, model, Document } from "mongoose";
 export interface INode extends Document {
   name: string;
   parentId?: string | null;
-  childlCount: number;
+  childCount: number;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -12,7 +12,7 @@ const nodeSchema = new Schema<INode>(
   {
     name: { type: String, required: true },
     parentId: { type: Schema.Types.ObjectId, ref: "Node", default: null },
-    childlCount: {type: Number, default: 0}
+    childCount: {type: Number, default: 0}
   },
   { timestamps: true }
 );
